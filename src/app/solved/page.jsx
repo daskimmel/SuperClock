d319b1e2c01c4c8728dbae560613097e99b76109
daskimmel1 <david.soth-kimmel@everstream.ai>
2023-09-10 20:50:20 -0600

    Add typewrite and video

--- src/app/solved/page.jsx
+++ src/app/solved/page.jsx
@@ -3,8 +3,23 @@ import styles from "./page.css"
 export default function Solved() {
 
   return (
-    <div className="solved-container">
-        You solved the puzzle
-    </div>
+    <>
+      <div className="solved-container">
+        <div className="line-1 anim-typewriter">
+            You solved the puzzle.
+        </div>
+      </div>
+      <div className="video-container fadein">
+        <iframe
+          width="853"
+          height="480"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded youtube"
+        />
+      </div>
+    </>
   )
 }
